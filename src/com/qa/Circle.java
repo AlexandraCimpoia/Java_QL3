760c60df6f3fa6c1a5034c4aa913925b76c03ae9
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-07-25 16:10:51 +0100

    Added Movable Interface

--- src/com/qa/Circle.java
+++ src/com/qa/Circle.java
@@ -1,6 +1,6 @@
 package com.qa;
 
-public class Circle extends Shape{
+public class Circle extends Shape implements Movable{
     private double radius;
 
     public Circle(String name, String colour, double x, double y,
@@ -26,5 +26,16 @@ public class Circle extends Shape{
     @Override
     public Point getCentrePoint() {
         return new Point(getX(), getY());
+    }
+
+    @Override
+    public Point getCurrentLocation() {
+        return getCentrePoint();
+    }
+
+    @Override
+    public void move(double x, double y) {
+        setX(getX() + x);
+        setY(getY() + y);
     }
 }
